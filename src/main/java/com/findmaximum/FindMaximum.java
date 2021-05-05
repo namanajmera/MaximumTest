f214cde6eb34e7dee9bfc5977317297d74e05ea5
Naman <cool.naman.ajmera@gmail.com>
2021-05-05 10:03:42 +0530

    Finding maximum in Double and pass all the test cases, of different positions.

--- src/main/java/com/findmaximum/FindMaximum.java
+++ src/main/java/com/findmaximum/FindMaximum.java
@@ -1,16 +1,24 @@
 package com.findmaximum;
 
 public class FindMaximum {
-    public Integer maximum(Integer num1, Integer num2, Integer num3)
-    {
-        Integer max=num1;
-        if (num2.compareTo(max)>0)
-        {
-            max=num2;
+    public Integer maximum(Integer num1, Integer num2, Integer num3) {
+        Integer max = num1;
+        if (num2.compareTo(max) > 0) {
+            max = num2;
         }
-        if (num3.compareTo(max)>0)
-        {
-            max=num3;
+        if (num3.compareTo(max) > 0) {
+            max = num3;
+        }
+        return max;
+    }
+
+    public Double maximum(Double num1, Double num2, Double num3) {
+        Double max = num1;
+        if (num2.compareTo(max) > 0) {
+            max = num2;
+        }
+        if (num3.compareTo(max) > 0) {
+            max = num3;
         }
         return max;
     }
--- src/test/java/com/findmaximum/FindMaximumTest.java
+++ src/test/java/com/findmaximum/FindMaximumTest.java
@@ -5,24 +5,48 @@ import org.junit.jupiter.api.Test;
 
 public class FindMaximumTest {
 
+
+    //    Test cases for Integer Maximum.
     @Test
     public void givenMaximumIntegerAtPosition1_ShouldReturnIntegerOfPosition1() {
         FindMaximum findMaximum = new FindMaximum();
-        Integer result=findMaximum.maximum(3,4,5);
-        Assertions.assertEquals(5,result);
+        Integer result = findMaximum.maximum(3, 4, 5);
+        Assertions.assertEquals(5, result);
     }
 
     @Test
     public void givenMaximumIntegerAtPosition2_ShouldReturnIntegerOfPosition2() {
         FindMaximum findMaximum = new FindMaximum();
-        Integer result=findMaximum.maximum(3,5,4);
-        Assertions.assertEquals(5,result);
+        Integer result = findMaximum.maximum(3, 5, 4);
+        Assertions.assertEquals(5, result);
     }
 
     @Test
     public void givenMaximumIntegerAtPosition3_ShouldReturnIntegerOfPosition3() {
         FindMaximum findMaximum = new FindMaximum();
-        Integer result=findMaximum.maximum(5,3,4);
-        Assertions.assertEquals(5,result);
+        Integer result = findMaximum.maximum(5, 3, 4);
+        Assertions.assertEquals(5, result);
+    }
+
+    //    Test cases for Floats Maximum
+    @Test
+    public void givenMaximumFloatAtPosition1_ShouldReturnFloatOfPosition1() {
+        FindMaximum findMaximum = new FindMaximum();
+        Double result = findMaximum.maximum(3.3, 4.4, 5.44);
+        Assertions.assertEquals(5.44, result);
+    }
+
+    @Test
+    public void givenMaximumFloatAtPosition2_ShouldReturnFloatOfPosition2() {
+        FindMaximum findMaximum = new FindMaximum();
+        Double result = findMaximum.maximum(3.3, 5.55, 4.4);
+        Assertions.assertEquals(5.55, result);
+    }
+
+    @Test
+    public void givenMaximumFloatAtPosition3_ShouldReturnFloatOfPosition3() {
+        FindMaximum findMaximum = new FindMaximum();
+        Double result = findMaximum.maximum(5.55, 3.33, 4.44);
+        Assertions.assertEquals(5.55, result);
     }
 }
